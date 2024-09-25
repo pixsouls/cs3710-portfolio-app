e0eec0cc830c3d753eca8b5c12c209db78e72588
Owen Pick <mercuryman1220@gmail.com>
2024-09-24 22:05:24 -0600

    some TDD cases

--- portfolio_app/test/models/student_test.rb
+++ portfolio_app/test/models/student_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should raise error when saving student without first name" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(school_email: "jokic@msudenver.edu", major: "CS", minor: "CS")
+    end
+  end
+
+  test "should raise error when saving student without an email" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(name: "Nikola", major: "CS", minor: "CS")
+    end
+  end
+
+  test "should raise error when saving student without major" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(name: "Nikola", school_email: "jokic@msudenver.edu", minor: "CS")
+    end
+  end
+
+  test "should raise error when saving student without minor" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(name: "Nikola", school_email: "jokic@msudenver.edu", major: "CS")
+    end
+  end
 end
